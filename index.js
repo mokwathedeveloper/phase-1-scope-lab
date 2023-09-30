4223cbcb7b66a0dd985ddf96d17da14f85f046f4
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-09-30 11:24:02 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function overwriteBestCustomer() {
+  TheBestCustomer = 'the best customer';
+}
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; // This will result in an error because you cannot reassign a value to a constant.
+}
